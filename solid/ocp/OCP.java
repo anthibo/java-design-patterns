e318afcc02085eb1e6bc3383ce812937942b27b0
anthibo <anthiboSama@gmail.com>
2023-01-01 22:17:46 +0200

    implement open closed principle

---
+++ solid/ocp/OCP.java
@@ -0,0 +1,125 @@
+package solid.ocp;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+enum Color {
+    RED, GREEN, BLUE
+}
+
+enum Size {
+    SMALL, MEDIUM, LARGE, YUGE
+}
+
+class Product {
+    public String name;
+    public Color color;
+    public Size size;
+
+    public Product(String name, Color color, Size size) {
+        this.name = name;
+        this.color = color;
+        this.size = size;
+    }
+}
+
+class ProductFilter {
+
+    public Stream<Product> filterByColor(List<Product> products, Color color) {
+        return products.stream().filter(p -> p.color == color);
+    }
+
+    public Stream<Product> filterBySize(List<Product> products, Size size) {
+        return products.stream().filter(p -> p.size == size);
+    }
+
+    public Stream<Product> filterBySizeAndColor(List<Product> products, Size size, Color color) {
+        return products.stream().filter(p -> p.size == size && p.color == color);
+    }
+}
+
+interface Specification<T> {
+    boolean isSatisfied(T item);
+}
+
+interface Filter<T> {
+    Stream<T> filter(List<T> items, Specification<T> spec);
+}
+
+class ColorSpecification implements Specification<Product> {
+    private Color color;
+
+    public ColorSpecification(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean isSatisfied(Product item) {
+        return item.color == color;
+    }
+
+}
+
+class SizeSpecification implements Specification<Product> {
+    private Size size;
+
+    public SizeSpecification(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean isSatisfied(Product item) {
+        return item.size == size;
+    }
+
+}
+
+class AndSpecification<T> implements Specification<T> {
+
+    private Specification<T> first, second;
+
+    public AndSpecification(Specification<T> first, Specification<T> second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean isSatisfied(T item) {
+        return first.isSatisfied(item) && second.isSatisfied(item);
+    }
+
+}
+
+class BetterProductFilter implements Filter<Product> {
+    @Override
+    public Stream<Product> filter(List<Product> items, Specification<Product> spec) {
+        return items.stream().filter(p -> spec.isSatisfied(p));
+    }
+
+}
+
+public class OCP {
+    public static void main(String[] args) {
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.LARGE);
+
+        List<Product> products = List.of(apple, tree, house);
+
+        ProductFilter pf = new ProductFilter();
+        System.out.println("Green products (old):");
+        pf.filterByColor(products, Color.GREEN).forEach(p -> System.out.println("- " + p.name + "is Green"));
+
+        BetterProductFilter bf = new BetterProductFilter();
+        System.out.println("Green products (new):");
+        bf.filter(products, new ColorSpecification(Color.GREEN))
+                .forEach(p -> System.out.println("- " + p.name + "is Green"));
+
+        System.out.println("Large Blue products:");
+        bf.filter(products,
+                new AndSpecification<Product>(new ColorSpecification(Color.BLUE), new SizeSpecification(Size.LARGE)))
+                .forEach(
+                        p -> System.out.println("- " + p.name + " is large blue item"));
+
+    }
+}
